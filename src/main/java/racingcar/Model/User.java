8176d25950daa0a562505bf995dee9bd97cbccb4
dev-msj <msj944@naver.com>
2022-04-25 19:54:58 +0900

    refector: IllegalArgumentException을 throws하도록 변경

--- src/main/java/racingcar/Model/User.java
+++ src/main/java/racingcar/Model/User.java
@@ -3,15 +3,18 @@ package racingcar.Model;
 import racingcar.Util.Validation;
 
 public class User {
-    private final String[] carNameArray;
-    private final int movingAmount;
+    private String[] carNameArray;
+    private int movingAmount;
 
-    public User(String carNames, String movingAmount) {
+    public void setCarNameArray(String carNames) {
         this.carNameArray = convertCarNames(carNames);
+    }
+
+    public void setMovingAmount(String movingAmount) {
         this.movingAmount = parseMovingAmount(movingAmount);
     }
 
-    private String[] convertCarNames(String carNames) {
+    private String[] convertCarNames(String carNames) throws IllegalArgumentException {
         String[] rawNameArray = carNames.split(",");
         for (String carName : rawNameArray) {
             validateCarName(carName);
@@ -20,13 +23,13 @@ public class User {
         return rawNameArray;
     }
 
-    private void validateCarName(String carName) {
+    private void validateCarName(String carName) throws IllegalArgumentException {
         if (!Validation.validateCarName(carName)) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 1-5자여야 한다.");
         }
     }
 
-    private int parseMovingAmount(String movingAmount) {
+    private int parseMovingAmount(String movingAmount) throws IllegalArgumentException {
         if (!Validation.validateMovement(movingAmount)) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 한다.");
         }
